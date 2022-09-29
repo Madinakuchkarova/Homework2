90965d2c6218e23e2f7f68adb27e17b7d0e0d433
Madisha <madinathotio@gmail.com>
2022-09-29 23:59:57 +0300

    Just commit for second homework

---
+++ src/Main.java
@@ -0,0 +1,69 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Home work2! ЗАДАЧА 1");
+        double dog = 8;
+        System.out.println(dog);
+        double cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        System.out.println("ЗАДАЧА 2");
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+        System.out.println("ЗАДАЧА 3");
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+        System.out.println("ЗАДАНИЕ 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend * 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+        System.out.println("ЗАДАНИЕ 5");
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        System.out.println("ЗАДАНИЕ 6");
+        var boxerFirst = 78.2;
+        var boxerSecond = 82.7;
+        var boxersWeight = boxerFirst + boxerSecond;
+        System.out.println("ОБЩИЙ ВЕС БОКСЕРОВ " + boxersWeight);
+        var weightDifference = boxerSecond - boxerFirst;
+        System.out.println("РАЗНИЦА В ВЕСЕ БОКСЕРОВ " + weightDifference);
+        System.out.println("ЗАДАНИЕ 7");
+        System.out.println("1 СПОСОБ РАЗНИЦА В ВЕСЕ БОКСЕРОВ " + weightDifference);
+        System.out.println("2 СПОСОБ РАЗНИЦА В ВЕСЕ БОКСЕРОВ ");
+        System.out.println("ЗАДАНИЕ 8");
+        var allTime = 640;
+        var timeForOne = 8;
+        var totalEmployees = allTime / timeForOne;
+        System.out.println("ВСЕГО РАБОТНИКОВ В КОМПАНИИ-" + totalEmployees + " ЧЕЛОВЕК");
+        System.out.println("ЗАДАНИЕ 8.2");
+        totalEmployees = totalEmployees + 94;
+        allTime = allTime / totalEmployees;
+        System.out.println("ЕСЛИ В КОМПАНИЕ РАБОТАЕТ " + totalEmployees + " ЧЕЛОВЕК,ТО ВСЕГО");
+        System.out.println( + allTime + " ЧАСА РАБОТЫ МОЖЕТ БЫТЬ ПОДЕЛЕНО МЕЖДУ СОТРУДНИКАМИ");
+
+
+
+
+
+
+
+
+    }
+}
