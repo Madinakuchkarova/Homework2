8580b6dadf012f275bf962f5da4cfeaa3ba5a75b
Madisha <madinathotio@gmail.com>
2022-10-01 17:18:04 +0300

    Changes for homework 2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Home work2! ЗАДАЧА 1");
-        double dog = 8;
+        var dog = 8;
         System.out.println(dog);
-        double cat = 3.6;
+        var cat = 3.6;
         System.out.println(cat);
         var paper = 763789;
         System.out.println(paper);
@@ -15,8 +15,9 @@ public class Main {
         paper = paper + 4;
         System.out.println(paper);
         System.out.println("ЗАДАЧА 3");
-        dog = dog - 3.5;
-        System.out.println(dog);
+        var dog1 = 8.0;
+        dog1 = dog1 - 3.5;
+        System.out.println(dog1);
         cat = cat - 1.6;
         System.out.println(cat);
         paper = paper - 7639;
@@ -24,7 +25,7 @@ public class Main {
         System.out.println("ЗАДАНИЕ 4");
         var friend = 19;
         System.out.println(friend);
-        friend = friend * 2;
+        friend = friend + 2;
         System.out.println(friend);
         friend = friend / 7;
         System.out.println(friend);
@@ -40,23 +41,21 @@ public class Main {
         System.out.println("ЗАДАНИЕ 6");
         var boxerFirst = 78.2;
         var boxerSecond = 82.7;
-        var boxersWeight = boxerFirst + boxerSecond;
-        System.out.println("ОБЩИЙ ВЕС БОКСЕРОВ " + boxersWeight);
-        var weightDifference = boxerSecond - boxerFirst;
-        System.out.println("РАЗНИЦА В ВЕСЕ БОКСЕРОВ " + weightDifference);
+        System.out.println("ОБЩИЙ ВЕС БОКСЕРОВ " + (boxerFirst + boxerSecond));
+        System.out.println("РАЗНИЦА В ВЕСЕ БОКСЕРОВ " + (boxerSecond - boxerFirst));
         System.out.println("ЗАДАНИЕ 7");
-        System.out.println("1 СПОСОБ РАЗНИЦА В ВЕСЕ БОКСЕРОВ " + weightDifference);
-        System.out.println("2 СПОСОБ РАЗНИЦА В ВЕСЕ БОКСЕРОВ ?");
+        System.out.println("1 СПОСОБ РАЗНИЦА В ВЕСЕ БОКСЕРОВ " + (boxerSecond - boxerFirst));
+        System.out.println("2 СПОСОБ РАЗНИЦА В ВЕСЕ БОКСЕРОВ " + (boxerSecond % boxerFirst));
         System.out.println("ЗАДАНИЕ 8");
-        var allTime = 640;
-        var timeForOne = 8;
-        var totalEmployees = allTime / timeForOne;
-        System.out.println("ВСЕГО РАБОТНИКОВ В КОМПАНИИ-" + totalEmployees + " ЧЕЛОВЕК");
+        var totalWorkingHours = 640;
+        var hoursPerDay = 8;
+        var totalEmployees = totalWorkingHours / hoursPerDay;
+        System.out.println("ВСЕГО РАБОТНИКОВ В КОМПАНИИ - " + totalEmployees + " ЧЕЛОВЕК");
         System.out.println("ЗАДАНИЕ 8.2");
         totalEmployees = totalEmployees + 94;
-        allTime = allTime / totalEmployees;
-        System.out.println("ЕСЛИ В КОМПАНИЕ РАБОТАЕТ " + totalEmployees + " ЧЕЛОВЕК,ТО ВСЕГО");
-        System.out.println( + allTime + " ЧАСА РАБОТЫ МОЖЕТ БЫТЬ ПОДЕЛЕНО МЕЖДУ СОТРУДНИКАМИ");
+        System.out.println("ЕСЛИ В КОМПАНИЕ РАБОТАЕТ " + totalEmployees  + " ЧЕЛОВЕК,ТО ВСЕГО");
+        totalWorkingHours = totalEmployees * hoursPerDay;
+        System.out.println( + totalWorkingHours + " ЧАСА РАБОТЫ МОЖЕТ БЫТЬ ПОДЕЛЕНО МЕЖДУ СОТРУДНИКАМИ");
 
 
 
